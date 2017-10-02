80c53ff57713f1f49df184b6fb403498ef757930
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2017-10-02 18:19:01 +0200

    Output

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,40 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class App extends Component {
-  componentDidMount () {
-    console.log('React running...')
+  constructor () {
+    super()
+    this.state = {
+      jokes: 4,
+      html: true,
+      text: ''
+    }
+  }
+
+  componentWillMount() {
+    this.getText()
+  }
+  
+  getText () {
+    axios.get(`http://api.icndb.com/jokes/random/${this.state.jokes}`)
+      .then( response => {
+        const text = response.data.value.map( ({joke}) => {
+          if (this.state.html) {
+            return `<p>${joke}</p>`
+          }
+          return joke
+        }).join('')
+        this.setState({ text }, () => console.log(this.state))
+      })
+      .catch( err => {
+        console.log(err);
+      })
   }
 
   render () {
     return (
       <div>
-        <h1>My App</h1>
+        <h1>My Super App</h1>
       </div>
     )
   }
